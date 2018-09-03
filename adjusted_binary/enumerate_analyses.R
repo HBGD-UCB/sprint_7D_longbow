e6a1cc591f9e410acfce1e58a0757a471c038206
Jeremy Coyle <jeremyrcoyle@gmail.com>
2018-09-03 16:24:27 -0700

    cleaned up adjusted binary enumeration

--- adjusted_binary/enumerate_analyses.R
+++ adjusted_binary/enumerate_analyses.R
@@ -21,16 +21,19 @@ id <- c("id")
 analyses <- expand.grid(A=A,Y=outcome_files$Y, stringsAsFactors = FALSE, KEEP.OUT.ATTRS = FALSE)
 analyses$id <- list(id)
 analyses$strata <- list(V)
+analyses$W <- adjustment_sets[analyses$A]
+names(analyses$W) <- NULL
+analyses$file <- outcome_files$file[match(analyses$Y,outcome_files$Y)]
+save(analyses, file="adjusted_binary_analyses.rdata")
 
 i=1
 for(i in 1:nrow(analyses)){
   analysis <- analyses[i,]
   analysis_params <- default_params
-  analysis_nodes <- as.list(analysis)
-  analysis_nodes$W <- adjustment_sets[[analysis_nodes$A]]
-  analysis_nodes$W <- gsub("W_bmi", "W_mbmi", analysis_nodes$W)
+  analysis_nodes <- as.list(analysis[c("W","A","Y","strata","id")])
+  analysis_nodes$W[[1]] <- gsub("W_bmi", "W_mbmi", analysis_nodes$W[[1]])
   analysis_params$nodes <- analysis_nodes
-  analysis_params$data$repository_path <- outcome_files$file[match(analysis_nodes$Y,outcome_files$Y)]
+  analysis_params$data$repository_path <- analysis$file
   inputs_filename <- sprintf("inputs/adjusted_binary_analysis_%03d.json",i)
   writeLines(toJSON(analysis_params),inputs_filename)
 }
